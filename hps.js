68bd25b01dd8cb24775b5ccad009f00258e0b7ec
ChervyachokMigo <github@svdgod.ru>
2024-08-28 14:32:22 +0500

    deps update

--- hps.js
+++ hps.js
@@ -1,7 +1,7 @@
 const log = console.log.bind(console)
 
-const fs = require('fs')
-const path = require('path')
+const fs = require('node:fs')
+const path = require('node:path')
 
 const exceljs = require('exceljs')
 const sqlite3 = require('better-sqlite3')
